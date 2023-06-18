ed314758164b6ef9e6de1c7ce8975ae259bafdf6
Sumit Kumar <skyworkid2011@gmail.com>
2023-06-19 00:15:34 +0530

    responsive page updates

--- src/App.js
+++ src/App.js
@@ -42,48 +42,44 @@ function App() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
     if (!recruiterInitials && !company) {
       setError("Please provide Recruiter's Initials and Company name");
       return;
     }
-  
+
     if (!recruiterInitials) {
       setError("Please enter Recruiter's Initials.");
       return;
     }
-  
+
     if (!company) {
       setError('Please enter Company Name.');
       return;
     }
-  
+
     setError('');
-  
+
     try {
       const fetchedData = await fetchSheetData(SPREADSHEET_ID, SHEET_RANGE, API_KEY);
-      
-      const filteredData = fetchedData.filter((record) =>{
 
-        const isRecruiterIntitals = record.recruiterInitials.toLowerCase().includes(recruiterInitials.toLowerCase()) 
-      
-        const isCompany = record.company.toLowerCase().includes(company.toLowerCase())
+      const filteredData = fetchedData.filter((record) => {
+        const isRecruiterIntitals = record.recruiterInitials.toLowerCase().includes(recruiterInitials.toLowerCase());
 
-        const isYear = (year === 'All Years' || parseInt(record.year) === parseInt(year))
+        const isCompany = record.company.toLowerCase().includes(company.toLowerCase());
 
-        return isRecruiterIntitals && isCompany && isYear
-        
-      }
-        
-      );
-  
+        const isYear = year === 'All Years' || parseInt(record.year) === parseInt(year);
+
+        return isRecruiterIntitals && isCompany && isYear;
+      });
+
       setGhostingRecords(filteredData);
-      setShowResults(true); 
+      setShowResults(true);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
   };
-  
+
   const years = [];
 
   for (let year = 2023; year >= 2012; year--) {
@@ -91,96 +87,102 @@ function App() {
   }
 
   return (
-    <Container
-      className="container mt-5 d-flex flex-column justify-content-center align-items-center"
-      style={{ minHeight: '30vh' }}
-    >
-      <h1 className="justify-content-center align-items-center" style={{ minHeight: '10vh' }}><b>Ghosting's Database</b></h1>
-      {error && (
-        <Alert variant="danger" className="mb-4">
-          {error}
-        </Alert>
-      )}
-      <Form>
-        <div className="row">
-          <div className="col">
-            <Form.Group>
-              <Form.Control
-                type="text"
-                value={recruiterInitials}
-                onChange={(e) => setRecruiterInitials(e.target.value)}
-                placeholder="Recruiter's Initials"
-                required
-              />
-            </Form.Group>
+    <div className="app-container">
+      <Container className="mt-5 d-flex flex-column justify-content-center align-items-center">
+        <h1 className="text-center" style={{ minHeight: '10vh' }}>
+          <b>Ghosting's Database</b>
+        </h1>
+        {error && (
+          <Alert variant="danger" className="mb-4">
+            {error}
+          </Alert>
+        )}
+        <Form>
+          <div className="row">
+            <div className="col-lg-3 col-md-6">
+              <Form.Group>
+                <Form.Control
+                  type="text"
+                  value={recruiterInitials}
+                  onChange={(e) => setRecruiterInitials(e.target.value)}
+                  placeholder="Recruiter's Initials"
+                  required
+                  className="form-control-sm"
+                />
+              </Form.Group>
+            </div>
+            <div className="col-lg-3 col-md-6">
+              <Form.Group>
+                <Form.Control
+                  type="text"
+                  value={company}
+                  onChange={(e) => setCompany(e.target.value)}
+                  placeholder="Company"
+                  required
+                  className="form-control-sm"
+                />
+              </Form.Group>
+            </div>
+            <div className="col-lg-3 col-md-6">
+              <Form.Group>
+                <Form.Control
+                  as="select"
+                  value={year}
+                  onChange={(e) => setYear(e.target.value)}
+                  className="form-control-sm"
+                >
+                  <option>All Years</option>
+                  {years.map((year) => (
+                    <option key={year} value={year}>
+                      {year}
+                    </option>
+                  ))}
+                </Form.Control>
+              </Form.Group>
+            </div>
+            <div className="col-lg-3 col-md-6">
+              <Button variant="primary" onClick={handleSubmit}>
+                Search
+              </Button>
+            </div>
           </div>
-          <div className="col">
-            <Form.Group>
-              <Form.Control
-                type="text"
-                value={company}
-                onChange={(e) => setCompany(e.target.value)}
-                placeholder="Company"
-                required
-              />
-            </Form.Group>
-          </div>
-          <div className="col">
-            <Form.Group>
-              <Form.Control
-                as="select"
-                value={year}
-                onChange={(e) => setYear(e.target.value)}
-              >
-                <option>All Years</option>
-                {years.map((year) => (
-                  <option key={year} value={year}>
-                    {year}
-                  </option>
-                ))}
-              </Form.Control>
-            </Form.Group>
-          </div>
-          <div className="col">
-            <Button variant="primary" onClick={handleSubmit}>
-              Search
-            </Button>
-          </div>
+        </Form>
+        <div className="table-responsive mt-4" style={{ maxWidth: '800px', margin: '0 auto' }}>
+          <Table striped bordered hover >
+            {showResults && ghostingRecords.length > 0 ? (
+              <thead>
+                <tr>
+                  <th>Company</th>
+                  <th>Recruiter's Initials</th>
+                  <th>Stage</th>
+                  <th>Follow Up</th>
+                  <th>Year</th>
+                </tr>
+              </thead>
+            ) : null}
+            <tbody>
+              {showResults && ghostingRecords.length > 0 ? (
+                ghostingRecords.map((record, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>{record.company}</td>
+                      <td>{record.recruiterInitials}</td>
+                      <td>{record.stage}</td>
+                      <td>{record.followUp}</td>
+                      <td>{record.year}</td>
+                    </tr>
+                  );
+                })
+              ) : (
+                <tr>
+                  <td colSpan="5">No records found.</td>
+                </tr>
+              )}
+            </tbody>
+          </Table>
         </div>
-      </Form>
-      <Table striped bordered hover className="mt-4" style={{ maxWidth: '800px' }}>
-  {showResults && ghostingRecords.length > 0 ? (
-    <thead>
-      <tr>
-        <th>Company</th>
-        <th>Recruiter's Initials</th>
-        <th>Stage</th>
-        <th>Follow Up</th>
-        <th>Year</th>
-      </tr>
-    </thead>
-  ) : null}
-  <tbody>
-    {showResults && ghostingRecords.length > 0 ? (
-      ghostingRecords.map((record, index) => {
-          return (
-            <tr key={index}>
-              <td>{record.company}</td>
-              <td>{record.recruiterInitials}</td>
-              <td>{record.stage}</td>
-              <td>{record.followUp}</td>
-              <td>{record.year}</td>
-            </tr>
-          );
-      })
-    ) : (
-      <tr>
-        <td colSpan="5">No records found.</td>
-      </tr>
-    )}
-  </tbody>
-</Table>
-    </Container>
+      </Container>
+    </div>
   );
 }
 
